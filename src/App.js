340e877f88f47b9488677af3a87314bd8a83df0a
polinashkurat <polinashkurat63@gmail.com>
2023-08-01 22:21:53 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Weather from "./Weather";
+
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>HAHA</h1>
+        <h1>Weather forecast: Düsseldorf </h1>
+
         <Weather />
       </div>
     </div>

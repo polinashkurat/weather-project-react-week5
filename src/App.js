b2828daa8a76d867629a99865f76f03577c1a81c
polinashkurat <polinashkurat63@gmail.com>
2023-08-01 22:07:36 +0200

    working on errors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <div className="container"></div>
+      <div className="container">
+        <h1>HAHA</h1>
+        <Weather />
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h2>Hallllooooo</h2>
+    </div>
+  );
+}

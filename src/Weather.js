d07e0ec1b851ea4ce9c0a2c4591a7ddbaf1316ed
polinashkurat <polinashkurat63@gmail.com>
2023-08-02 14:30:12 +0200

    Changed html and added css

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
   return (
     <div className="Weather">
-      <h2>Hallllooooo</h2>
+      <h2>Düsseldorf</h2>
+      <ul>
+        <li>Wednesday, 11:00</li>
+        <li>Light rain showers</li>
+      </ul>
     </div>
   );
 }

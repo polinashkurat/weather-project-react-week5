fd7b1765a011d25a235ff06b6cbfbc796227c7cb
polinashkurat <polinashkurat63@gmail.com>
2023-08-03 19:00:21 +0200

    Added basic css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Weather from "./Weather";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather forecast: Düsseldorf </h1>
-
         <Weather />
+        <footer>
+          This project was coded by Polina Shkurat and is{" "}
+          <a
+            href="https://github.com/polinashkurat/weather-project-react-week5"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -4,11 +4,51 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
       <h2>Düsseldorf</h2>
       <ul>
         <li>Wednesday, 11:00</li>
         <li>Light rain showers</li>
       </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+            <div className="clearfix">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
+            alt="light rain showers" className="float-left"
+          />
+          <div className="float-left">
+          <span className="temperature">18</span>
+          <span className="unit">°C </span>
+        </div>
+        </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 60%</li>
+            <li>Humidity: 40%</li>
+            <li>Wind: 12 km/h </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }

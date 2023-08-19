e3fa10fc8589c3e4ec8f994ba80fd482d2d8bf90
polinashkurat <polinashkurat63@gmail.com>
2023-08-19 12:49:32 +0200

    Fixed the issue with API

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -34,10 +34,10 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "2daf65f0cdaa917f11026e8a128ce271";
+    let apiKey = "50fa4024e3b1d5eac2f51ab18a47e997";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
 
     return null;
